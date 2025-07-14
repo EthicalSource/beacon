197b9f52000563e935f81ad2a7a48a954590b0c4
CoralineAda <coraline@idolhands.com>
2025-07-14 14:49:18 -0500

    Add SENDGRID env variables

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,10 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  password: ENV['SENDGRID_API_KEY'],
   domain: 'coc-beacon.com',
   host: 'www.coc-beacon.com',
-  address: 'smtp.sendgrid.net',
+  address: ENV['SENDGRID_SERVER'],
   port: 587,
   authentication: :plain,
   enable_starttls_auto: true
